ac9060c8e1c2897ae5d99b363416972e5061c8a4
MaelTriquet <mael1triquet@gmail.com>
2024-12-17 12:36:05 +0100

    grid testing

--- src/grid.py
+++ src/grid.py
@@ -1,5 +1,4 @@
 from cell import Cell
-
 class Grid:
     cells : list[list[Cell]]
     radius : float
@@ -8,8 +7,8 @@ class Grid:
 
     def __init__(self, win_width, win_height, radius_):
         self.radius = radius_
-        self.width = int(win_width) // radius_ + 1
-        self.height = int(win_height) // radius_ + 1
+        self.width = int(win_width - 1e-5) // radius_ + 1
+        self.height = int(win_height - 1e-5) // radius_ + 1
         self.cells = [[Cell(j + i * self.width) for j in range(self.width)] for i in range(self.height)]
 
     def update(self, pop : list):
@@ -22,8 +21,8 @@ class Grid:
         for cell in self.cells:
             cell.clear()
         for element in pop:
-            x = int(element.pos[0])
-            y = int(element.pos[1])
+            x = int(element.position[0])
+            y = int(element.position[1])
             self.cells[x // self.radius + y // self.radius * self.width].add(element) # maybe element.index??
 
     def get_neighbours(self, index) -> list[Cell]:
@@ -44,3 +43,45 @@ class Grid:
         res.append(self.cells[(i + 1) % self.height][(j + 1) % self.width])                            # bottom right
 
         return res
+    
+def test():
+
+    #test init 1
+    grid = Grid(800, 800, 100)
+    if (len(grid.cells) != 8 or len(grid.cells[0]) != 8):
+        print("Error : size 1 fail")
+    else:
+        print("Success : size 1 correct")
+
+    #test init 2
+    grid = Grid(901, 901, 100)
+    if (len(grid.cells) != 10 or len(grid.cells[0]) != 10):
+        print("Error : size 2 fail")
+    else:
+        print("Success : size 2 correct")
+
+    #test neighbours
+    neighbours = grid.get_neighbours(11) # 1, 1
+    neighbours_idx = [n.index for n in neighbours]
+    if not 0 in neighbours_idx:
+        print("Error : top left")
+    elif not 1 in neighbours_idx:
+        print("Error : top")
+    elif not 2 in neighbours_idx:
+        print("Error : top right")
+    elif not 10 in neighbours_idx:
+        print("Error : left")
+    elif not 12 in neighbours_idx:
+        print("Error : right")
+    elif not 20 in neighbours_idx:
+        print("Error : bot left")
+    elif not 21 in neighbours_idx:
+        print("Error : bot")
+    elif not 22 in neighbours_idx:
+        print("Error : bot right")
+    elif not 11 in neighbours_idx:
+        print("Error : self")
+    else:
+        print("Success : neighbours correct")
+if __name__ == "__main__":
+    test()
