eadf580cbe3693c5a79b35eead9909acc89b906e
MaelTriquet <mael1triquet@gmail.com>
2024-12-17 10:09:16 +0100

    basic rendering

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from processing_py import *
+
+def main():
+    app = App(800, 800)
+    while True:
+        draw(app)
+
+    
+def draw(app):
+    app.background(0)
+
+
+if __name__ == "__main__":
+    main()
