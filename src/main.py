d60954ae8166679cc1e71f585b157a63b3face53
MaelTriquet <mael1triquet@gmail.com>
2024-12-17 10:15:56 +0100

    pretty much fianl main

--- src/main.py
+++ src/main.py
@@ -2,12 +2,19 @@ from processing_py import *
 
 def main():
     app = App(800, 800)
+    simulation = Simulation(app)
     while True:
+        simulation.update()
+        simulation.show()
         draw(app)
+
+# app.fill(r, g, b)
+# app.circle(pos_x, pos_y, rayon)
 
     
 def draw(app):
-    app.background(0)
+    app.background(120)
+    app.redraw()
 
 
 if __name__ == "__main__":
