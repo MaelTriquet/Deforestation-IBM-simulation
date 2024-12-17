4c985e0a007a65d252268c2440a86714b67e2fca
MaelTriquet <mael1triquet@gmail.com>
2024-12-17 15:14:50 +0100

    random generator

---
+++ c++/src/Random.hpp
@@ -0,0 +1,11 @@
+#include <random>
+#include <cmath>
+
+class Random {
+    static std::random_device rd;
+    static std::mt19937 gen;
+    static std::uniform_real_distribution<> dis;
+    static float rand();
+    static int randint(int max);
+    static int randint(int min, int max);
+};
---
+++ c++/src/random.cpp
@@ -0,0 +1,17 @@
+#include "Random.hpp"
+
+std::random_device Random::rd;
+std::mt19937 Random::gen(rd());
+std::uniform_real_distribution<> Random::dis(0.0, 1.0);
+
+float Random::rand() {
+    return dis(gen);
+}
+
+int Random::randint(int max) {
+    return std::floor(dis(gen) * max);
+}
+
+int Random::randint(int min, int max) {
+    return std::floor(dis(gen) * (max + min) - min);
+}
