e685ebca509ef0feaa84ec1ae5d15df2b148344f
MaelTriquet <mael1triquet@gmail.com>
2024-12-17 15:00:45 +0100

    cleaning up rendering

--- c++/src/Renderer.cpp
+++ c++/src/Renderer.cpp
@@ -1,6 +1,15 @@
-class Renderer {
-private:
+#include "renderer.hpp"
 
-public:
+explicit
+Renderer::Renderer(sf::RenderTarget& target):
+    m_target{target}
+{}
 
-};
+void Renderer::render(const Simulation& simulation) const {
+    for (const Animal& a : simulation.m_pop) {
+        sf::CircleShape shape(a.radius);
+        shape.setFillColor(sf::Color(255, 0, 0));
+        shape.setPosition(a.position);
+        m_target.draw(shape);
+    }
+}
--- c++/src/renderer.hpp
+++ c++/src/renderer.hpp
@@ -6,21 +6,9 @@ class Renderer
 {
 public:
     explicit
-    Renderer(sf::RenderTarget& target)
-        : m_target{target}
-    {
+    Renderer(sf::RenderTarget& target);
 
-    }
-
-    void render(const Simulation& simulation) const
-    {
-        for (const Animal& a : simulation.m_pop) {
-            sf::CircleShape shape(a.radius);
-            shape.setFillColor(sf::Color(255, 0, 0));
-            shape.setPosition(a.position);
-            m_target.draw(shape);
-        }
-    }
+    void render(const Simulation& simulation) const;
 
 private:
     sf::RenderTarget& m_target;
