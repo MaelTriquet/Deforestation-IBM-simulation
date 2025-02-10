882b685c8c8c1d87f16fb21cf8ea06b81daebdff
Hugo <hugo.hrst3@gmail.com>
2025-02-10 13:48:32 +0100

    resolving gitignore problem (?)

--- c++/build/CMakeFiles/MySFMLProject.dir/src/Predator.cpp.o(1).d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MySFMLProject.dir/src/Predator.cpp.o: \
- /home/cytech/Desktop/PFE/Deforestation-IBM-simulation/c++/src/Predator.cpp \
- /usr/include/stdc-predef.h
--- c++/build/CMakeFiles/MySFMLProject.dir/src/Renderer.cpp.o(1).d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/MySFMLProject.dir/src/Renderer.cpp.o: \
- /home/cytech/Desktop/PFE/Deforestation-IBM-simulation/c++/src/Renderer.cpp \
- /usr/include/stdc-predef.h
